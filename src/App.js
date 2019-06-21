ce0d8a2fbe94ac73c814bd9d12e12d6462d68ade
Derek <derek.baker89@gmail.com>
2019-06-21 03:18:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Book from "./Book";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Harry Potter");
+
+  useEffect(() => {
+    getBooks();
+  }, [query]);
+
+  const getBooks = async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
+    );
+    const data = await response.json();
+    setBooks(data.items);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="books">
+        {books.map(book => (
+          <Book
+            key={book.id}
+            title={book.volumeInfo.title}
+            authors={book.volumeInfo.authors}
+            publisher={book.volumeInfo.publisher}
+            image={book.volumeInfo.imageLinks.thumbnail}
+            info={book.volumeInfo.infoLink}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
