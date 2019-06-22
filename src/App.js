54c30896514a4413c3232a9c202faa8e7ffb6fc9
Derek <derek.baker89@gmail.com>
2019-06-22 08:23:13 +0100

    added api key

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   const getBooks = async () => {
     const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}`
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
     );
     const data = await response.json();
     setBooks(data.items);
