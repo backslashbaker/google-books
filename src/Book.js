6fcf721892559a1ef2899983d9fc496f5d60edbf
Derek Baker <derek@dereks-mbp.home>
2019-07-03 23:30:32 +0100

    fix console errors about <Li> keys

--- src/Book.js
+++ src/Book.js
@@ -2,12 +2,17 @@ import React from "react";
 import style from "./book.module.css";
 
 const Book = ({ title, authors, publisher, image, info }) => {
+  let counter = 1;
   return (
     <div className={style.book}>
       <h2 className="title">{title}</h2>
       <ul>
         {authors &&
-          authors.map(author => <li className="authors">{author}</li>)}
+          authors.map(author => (
+            <li key={"myKey" + counter++} className="authors">
+              {author}
+            </li>
+          ))}
       </ul>
       <p className="publisher">Publisher: {publisher && publisher}</p>
       <img src={image} alt="" className="image" />
